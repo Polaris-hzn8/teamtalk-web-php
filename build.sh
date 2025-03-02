c3627af8b5df41c618ba0877c3cd7fdca911c3b2
luochenhao <3453851623@qq.com>
2025-03-02 14:01:25 +0800

    feat: deploy succeed.

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# 压缩打包
-zip -r auto/src.zip src
-
-cd auto
--- src/setup.sh
+++ src/setup.sh
@@ -2,13 +2,14 @@
 # this is a setup scripts for web
 
 # setup web
-PHP_WEB=tt
-PHP_WEB_SETUP_PATH=/var/www/html
+PHP_WEB=im-server-web
+PHP_WEB_SETUP_PATH=/var/www/html        # web代码路径
+PHP_NGINX_CONF_PATH=/usr/local/nginx/conf/conf.d
+PHP_DB_CONF_PATH=$PHP_WEB_SETUP_PATH/$PHP_WEB/application/config
+
 PHP_DB_CONF=database.php
 PHP_MSFS_CONF=config.php
-PHP_DB_CONF_PATH=$PHP_WEB_SETUP_PATH/$PHP_WEB/application/config
 PHP_NGINX_CONF=im.com.conf
-PHP_NGINX_CONF_PATH=/usr/local/nginx/conf/conf.d
 
 print_hello(){
     echo "==========================================="
@@ -24,33 +25,39 @@ check_user() {
 }
 
 build_web(){
-    if [ -d $PHP_WEB ]; then
-        echo "$PHP_WEB has existed."
-    else
-        unzip $PHP_WEB.zip
-        if [ $? -eq 0 ]; then
-            echo "unzip $PHP_WEB successed."
-        else
-            echo "Error: unzip $PHP_WEB failed."
-        return 1
-        fi
-    fi
+    # 解压源码包
+    # if [ -d $PHP_WEB ]; then
+    #     echo "$PHP_WEB has existed."
+    # else
+    #     unzip $PHP_WEB.zip
+    #     if [ $? -eq 0 ]; then
+    #         echo "unzip $PHP_WEB successed."
+    #     else
+    #         echo "Error: unzip $PHP_WEB failed."
+    #         return 1
+    #     fi
+    # fi
 
+    # 源码部署 Nginx-Web目录
     set -x
     mkdir -p $PHP_WEB_SETUP_PATH
-    cp -r $PHP_WEB/ $PHP_WEB_SETUP_PATH
-    cp ./conf/$PHP_DB_CONF $PHP_DB_CONF_PATH/
-    cp ./conf/$PHP_MSFS_CONF $PHP_DB_CONF_PATH/
+    cp -r ./* $PHP_WEB_SETUP_PATH
+    cp ./tools/$PHP_DB_CONF $PHP_DB_CONF_PATH/
+    cp ./tools/$PHP_MSFS_CONF $PHP_DB_CONF_PATH/
     set +x
 
+    # 移除 default.conf
     if [ -f $PHP_NGINX_CONF_PATH/default.conf ]; then
         rm $PHP_NGINX_CONF_PATH/default.conf
         echo "remove $PHP_NGINX_CONF_PATH/default.conf successed."
     fi
+
+    # Nginx相关配置
     set -x
-    cp ./conf/$PHP_NGINX_CONF $PHP_NGINX_CONF_PATH/
+    cp ./tools/$PHP_NGINX_CONF $PHP_NGINX_CONF_PATH/
     chmod -R 777 $PHP_WEB_SETUP_PATH/$PHP_WEB/
     set +x
+
     systemctl stop nginx.service 
     systemctl start nginx.service 
     return 0
@@ -71,6 +78,10 @@ case $1 in
         print_hello $1
         check_user
         build_web
+        ;;
+    pack)
+        cd ../
+        zip -r $PHP_WEB.zip src
         ;;
     *)
     print_help
